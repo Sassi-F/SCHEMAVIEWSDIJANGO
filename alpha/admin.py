d0b0ae6069964e95f94b5fb51f0dfa69ac0bb620
FIZA <frmughal83@gmail.com>
2026-08-20 13:26:59 +0500

    medicien

--- alpha/admin.py
+++ alpha/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from alpha.models import Doctor, Patient, Appointment, LabTest
+from alpha.models import Doctor, Patient, Appointment, LabTest, Prescription, Bill, Medicine, MedicalRecord
 
 
 # Register Doctor Model
@@ -17,14 +17,35 @@ class PatientAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 admin.site.register(Patient, PatientAdmin)
 
-
 # Register Appointment Model
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = ('id', 'patient', 'doctor', 'remarks', 'status', 'created_at', 'updated_at')
 admin.site.register(Appointment, AppointmentAdmin)
 
 # Register Appointment Model
-class labtestAdmin(admin.ModelAdmin):
-    list_display = ('Test', 'Price', 'Discription', 'created_at', 'updated_at')
-    search_fields = ('Test', 'Price', 'Discription', 'created_at', 'updated_at')
-admin.site.register(LabTest, labtestAdmin)
+class LabtestAdmin(admin.ModelAdmin):
+    list_display = ('Test', 'Price', 'Description', 'created_at', 'updated_at',)
+    search_fields = ('Test',)
+admin.site.register(LabTest, LabtestAdmin)
+
+class Medicineadmin(admin.ModelAdmin):
+    list_display = ('id','medicine_name','strength','price','stock','created_at', 'updated_at',)
+admin.site.register(Medicine, Medicineadmin)
+
+# Register Prescription Model
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'appointment', 'notes', 'created_at', 'updated_at',)
+    search_fields = ('notes',)
+
+admin.site.register(Prescription, PrescriptionAdmin)
+
+class Medicalrecordadmin(admin.ModelAdmin):
+    list_display = ( 'id','patient' , 'Appointment','diagnosis' ,'allergies' ,'medical_history','notes','created_at','updated_at' ,)
+admin.site.register(MedicalRecord, Medicalrecordadmin)
+
+class Billadmin(admin.ModelAdmin):
+    list_display = ('doctor_fee','Appointment','lab_total','medicine_total','discount','tax','grand_total','payment_status','payment_date' , 'created_at', 'updated_at',)
+    search_fields = ('lab_total',)
+
+admin.site.register(Bill, Billadmin)
+
--- alpha/models.py
+++ alpha/models.py
@@ -42,6 +42,16 @@ class Patient(models.Model):
     def __str__(self):
         return f"Patient {self.name}"
 
+class LabTest(models.Model):
+    Test = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=6, decimal_places=2)
+    Description = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"LabTest {self.Test}"
 class Appointment(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
@@ -53,7 +63,7 @@ class Appointment(models.Model):
         ("Cancelled", "Cancelled"),
     ]
     status = models.CharField(max_length=255, choices=APPOINTMENT_STATUS_CHOICES)
-
+    lab_tests = models.ManyToManyField(LabTest)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -61,15 +71,60 @@ class Appointment(models.Model):
 def __str__(self):
     return f"Appointment {self.patient.name} with {self.doctor.name} at {self.date_time}"
 
+class Medicine(models.Model):
+    id= models.AutoField(primary_key=True)
+    medicine_name = models.CharField(max_length=255)
+    strength = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.id}"
 
-class LabTest(models.Model):
-    Test = models.CharField(max_length=255)
-    Price = models.DecimalField(max_digits=6, decimal_places=2)
-    Discription = models.TextField(null=True, blank=True)
+class Prescription(models.Model):
+    appointment = models.OneToOneField(Appointment, on_delete=models.CASCADE)
+    notes = models.TextField(null=True, blank=True)
+    Medicine = models.ManyToManyField(Medicine)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id}"
+
+class MedicalRecord(models.Model):
+    id= models.AutoField(primary_key=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    Appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    diagnosis = models.TextField(null=True, blank=True)
+    allergies = models.TextField(null=True, blank=True)
+    medical_history = models.TextField(null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id}"
+
+class Bill(models.Model):
+    doctor_fee= models.DecimalField(max_digits=6, decimal_places=2)
+    Appointment = models.ForeignKey(LabTest, on_delete=models.CASCADE, null=True, blank=True, related_name="lab_tests")
+    lab_test = models.ForeignKey(LabTest, on_delete=models.CASCADE)
+    lab_total = models.DecimalField(max_digits=6, decimal_places=2)
+    medicine = models.DecimalField(max_digits=6, decimal_places=2)
+    medicine_total = models.DecimalField(max_digits=6, decimal_places=2)
+    discount = models.DecimalField(max_digits=6, decimal_places=2)
+    tax= models.DecimalField(max_digits=6, decimal_places=2)
+    grand_total = models.DecimalField(max_digits=6, decimal_places=2)
+    PAYMENT_STATUS_CHOICES = [
+        ("P", "Paid"),
+        ("u", "Unpaid"),
+    ]
+    payment_status = models.CharField(max_length=6, choices=PAYMENT_STATUS_CHOICES)
+    payment_date = models.DateTimeField(auto_now_add=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-def __str__(self):
-    return f"LabTest {self.Test}"
+    def __str__(self):
+        return f"{self.id}"
